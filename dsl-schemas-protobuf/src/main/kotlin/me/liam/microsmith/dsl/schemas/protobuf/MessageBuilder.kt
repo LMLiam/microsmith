f4f3c2de0899220e610add87bcaaefdf466c1b44
LMLiam <46268350+TheRealEmissions@users.noreply.github.com>
2025-10-27 14:07:16 +0000

    Validate index

--- dsl-schemas-protobuf/src/main/kotlin/me/liam/microsmith/dsl/schemas/protobuf/MessageBuilder.kt
+++ dsl-schemas-protobuf/src/main/kotlin/me/liam/microsmith/dsl/schemas/protobuf/MessageBuilder.kt
@@ -1,23 +1,44 @@
 package me.liam.microsmith.dsl.schemas.protobuf
 
 class MessageBuilder(private val name: String) : MessageScope {
-    private val fields = mutableSetOf<Field>()
-    private var fieldNumber = 1
+    private val fields = mutableMapOf<String, Field>()
+    private val usedIndexes = mutableSetOf<Int>()
+    private var fieldIndex = 1
 
-    override fun int32(name: String, block: FieldScope.() -> Unit) {
-        val builder = FieldBuilder(fieldNumber, false).apply(block)
-        addField(name, builder, FieldType.INT32)
+    override fun optional(field: Field) {
+        fields[field.name] = field.copy(optional = true)
     }
 
-    override fun string(name: String, block: FieldScope.() -> Unit) {
-        val builder = FieldBuilder(fieldNumber, false).apply(block)
-        addField(name, builder, FieldType.STRING)
+    override fun int32(name: String, block: FieldScope.() -> Unit): Field {
+        return addField(name, FieldType.INT32, block)
     }
 
-    private fun addField(name: String, builder: FieldBuilder, type: FieldType) {
-        fields += Field(name, type, builder.index, builder.optional)
-        fieldNumber++
+    override fun string(name: String, block: FieldScope.() -> Unit): Field {
+        return addField(name, FieldType.STRING, block)
     }
 
-    fun build() = Message(name, fields)
+    private fun addField(name: String, type: FieldType, block: FieldScope.() -> Unit): Field {
+        require(name.isNotBlank()) { "Field name cannot be blank" }
+        require(!fields.containsKey(name)) { "Duplicate field name: $name" }
+
+        val builder = FieldBuilder(fieldIndex, false).apply(block)
+
+        validateIndex(builder.index)
+
+        require(!usedIndexes.contains(builder.index)) { "Duplicate field number: ${builder.index}" }
+
+        val field = Field(name, type, builder.index, builder.optional)
+        fields[name] = field
+        usedIndexes += builder.index
+
+        fieldIndex = maxOf(fieldIndex + 1, builder.index + 1)
+        return field
+    }
+
+    private fun validateIndex(index: Int) {
+        require(index in 1..536_870_911) { "Invalid field number: $index" }
+        require(index !in 19_000..19_999) { "Reserved field number: $index" }
+    }
+
+    fun build() = Message(name, fields.values.toSet())
 }
