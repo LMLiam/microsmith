5e5242be58503e8707d0f171357e947de7785e96
LMLiam <me@liammartin.site>
2025-10-29 15:31:59 +0000

    simpler generation

--- gen/src/main/kotlin/me/liam/microsmith/gen/files/FileSpace.kt
+++ gen/src/main/kotlin/me/liam/microsmith/gen/files/FileSpace.kt
@@ -4,8 +4,4 @@ import java.nio.file.Path
 
 sealed interface FileSpace {
     val root: Path
-    fun resolve(relative: String) = root.resolve(relative)
 }
-
-data class TemporaryFileSpace(override val root: Path) : FileSpace
-data class SourceFileSpace(override val root: Path) : FileSpace
