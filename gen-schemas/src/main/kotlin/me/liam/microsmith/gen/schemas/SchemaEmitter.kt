0b651da042f2ddc0d807791eb13870d1289454e7
LMLiam <46268350+TheRealEmissions@users.noreply.github.com>
2025-10-29 22:21:28 +0000

    use kclass for type

--- gen-schemas/src/main/kotlin/me/liam/microsmith/gen/schemas/SchemaEmitter.kt
+++ gen-schemas/src/main/kotlin/me/liam/microsmith/gen/schemas/SchemaEmitter.kt
@@ -3,9 +3,10 @@ package me.liam.microsmith.gen.schemas
 import me.liam.microsmith.dsl.schemas.core.Schema
 import me.liam.microsmith.gen.files.FileSpace
 import me.liam.microsmith.gen.files.GeneratedFile
+import kotlin.reflect.KClass
 
 interface SchemaEmitter<T : Schema> {
-    val type: T
+    val type: KClass<T>
 
     suspend fun T.emit(space: FileSpace): GeneratedFile
 }
