29b2ec97df210228f66eb45d630fd2d98c6aa897
LMLiam <46268350+TheRealEmissions@users.noreply.github.com>
2025-10-30 13:40:59 +0000

    start of gen-schemas-protobuf

--- gen-schemas/src/main/kotlin/me/liam/microsmith/gen/schemas/SchemaEmitter.kt
+++ gen-schemas/src/main/kotlin/me/liam/microsmith/gen/schemas/SchemaEmitter.kt
@@ -1,10 +1,12 @@
 package me.liam.microsmith.gen.schemas
 
+import com.github.eventhorizonlab.spi.ServiceContract
 import me.liam.microsmith.dsl.schemas.core.Schema
 import me.liam.microsmith.gen.files.FileSpace
 import me.liam.microsmith.gen.files.GeneratedFile
 import kotlin.reflect.KClass
 
+@ServiceContract
 interface SchemaEmitter<T : Schema> {
     val type: KClass<T>
 
--- gen-schemas/src/main/kotlin/me/liam/microsmith/gen/schemas/SchemasGenerator.kt
+++ gen-schemas/src/main/kotlin/me/liam/microsmith/gen/schemas/SchemasGenerator.kt
@@ -1,26 +1,25 @@
 package me.liam.microsmith.gen.schemas
 
 import com.github.eventhorizonlab.spi.ServiceProvider
-import kotlinx.coroutines.async
 import me.liam.microsmith.dsl.schemas.core.Schema
 import me.liam.microsmith.dsl.schemas.core.SchemasExtension
 import me.liam.microsmith.gen.core.ModelGenerator
 import me.liam.microsmith.gen.files.FileSpace
 import me.liam.microsmith.gen.files.GeneratedFile
-import kotlin.reflect.KClass
+import java.util.*
 
 @ServiceProvider(ModelGenerator::class)
-class SchemasGenerator(
-    private val emitters: Set<SchemaEmitter<out Schema>>
-) : ModelGenerator<SchemasExtension> {
+class SchemasGenerator : ModelGenerator<SchemasExtension> {
     override val extension get() = SchemasExtension::class
 
-    override suspend fun SchemasExtension.generate(space: FileSpace) =
-        schemas.map { schema ->
+    override suspend fun SchemasExtension.generate(space: FileSpace): List<GeneratedFile> {
+        val emitters = ServiceLoader.load(SchemaEmitter::class.java)
+        return schemas.map { schema ->
             val emitter =
                 emitters.firstOrNull { it.type == schema::class }
                     ?: error("No emitter found for schema type: ${schema::class}")
             @Suppress("UNCHECKED_CAST")
             (emitter as SchemaEmitter<Schema>).run { schema.emit(space) }
         }
+    }
 }
