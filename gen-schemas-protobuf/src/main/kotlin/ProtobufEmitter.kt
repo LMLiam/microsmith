1012fa032e8c55233fb791f5bfcca41e90ad9c88
LMLiam <46268350+TheRealEmissions@users.noreply.github.com>
2025-10-30 13:44:42 +0000

    start of protobuf emitter

---
+++ gen-schemas-protobuf/src/main/kotlin/ProtobufEmitter.kt
@@ -0,0 +1,15 @@
+import com.github.eventhorizonlab.spi.ServiceProvider
+import me.liam.microsmith.dsl.schemas.protobuf.ProtobufSchema
+import me.liam.microsmith.gen.files.FileSpace
+import me.liam.microsmith.gen.files.GeneratedFile
+import me.liam.microsmith.gen.schemas.SchemaEmitter
+import kotlin.reflect.KClass
+
+@ServiceProvider(SchemaEmitter::class)
+class ProtobufEmitter(
+    override val type: KClass<ProtobufSchema> = ProtobufSchema::class
+) : SchemaEmitter<ProtobufSchema> {
+    override suspend fun ProtobufSchema.emit(space: FileSpace): GeneratedFile {
+        TODO("Not yet implemented")
+    }
+}
