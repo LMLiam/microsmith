78c798c4e5d56814a04607c8956c09164e40ba2c
LMLiam <me@liammartin.site>
2025-12-13 17:39:43 +0000

    feat: Add JVM dependencies for Microsmith scripting support

--- scripting/src/main/kotlin/me/liam/microsmith/scripting/MicrosmithScript.kt
+++ scripting/src/main/kotlin/me/liam/microsmith/scripting/MicrosmithScript.kt
@@ -6,6 +6,8 @@ import kotlin.script.experimental.annotations.KotlinScript
 import kotlin.script.experimental.api.ScriptCompilationConfiguration
 import kotlin.script.experimental.api.defaultImports
 import kotlin.script.experimental.api.implicitReceivers
+import kotlin.script.experimental.jvm.dependenciesFromClassloader
+import kotlin.script.experimental.jvm.jvm
 
 @KotlinScript(
     fileExtension = "microsmith.kts",
@@ -19,6 +21,12 @@ abstract class MicrosmithScript(
 
 object MicrosmithScriptDefinition : ScriptCompilationConfiguration(
     {
+        jvm {
+            dependenciesFromClassloader(
+                "kotlin-stdlib",
+                "kotlin-reflect"
+            )
+        }
         defaultImports(
             "me.liam.microsmith.dsl.core.*",
             "me.liam.microsmith.dsl.schemas.core.*",
