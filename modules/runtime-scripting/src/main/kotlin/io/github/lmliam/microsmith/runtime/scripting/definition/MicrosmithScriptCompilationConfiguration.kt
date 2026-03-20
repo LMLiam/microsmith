04a6f62ff59d7d1e5fe5acfbd7a94671e5159dc8
LMLiam <46268350+TheRealEmissions@users.noreply.github.com>
2026-03-20 20:50:38 +0000

    fix(runtime-scripting): import rpc service from rpc package

--- modules/runtime-scripting/src/main/kotlin/io/github/lmliam/microsmith/runtime/scripting/definition/MicrosmithScriptCompilationConfiguration.kt
+++ modules/runtime-scripting/src/main/kotlin/io/github/lmliam/microsmith/runtime/scripting/definition/MicrosmithScriptCompilationConfiguration.kt
@@ -6,6 +6,7 @@ import io.github.lmliam.microsmith.dsl.schemas.core.SchemasScope
 import io.github.lmliam.microsmith.dsl.schemas.core.schemas
 import io.github.lmliam.microsmith.dsl.schemas.protobuf.ProtobufScope
 import io.github.lmliam.microsmith.dsl.schemas.protobuf.protobuf
+import io.github.lmliam.microsmith.dsl.schemas.protobuf.rpc.ServiceScope
 import io.github.lmliam.microsmith.dsl.schemas.protobuf.rpc.service
 import io.github.lmliam.microsmith.runtime.scripting.context.MicrosmithScriptContext
 import kotlin.reflect.KClass
@@ -23,7 +24,7 @@ object MicrosmithScriptCompilationConfiguration : ScriptCompilationConfiguration
             importFromPackageOf(MicrosmithScope::class, ::microsmith),
             importFromPackageOf(SchemasScope::class, MicrosmithScope::schemas),
             importFromPackageOf(ProtobufScope::class, SchemasScope::protobuf),
-            importFromPackageOf(ProtobufScope::class, ProtobufScope::service),
+            importFromPackageOf(ServiceScope::class, ProtobufScope::service),
         )
 
         implicitReceivers(MicrosmithScriptContext::class)
